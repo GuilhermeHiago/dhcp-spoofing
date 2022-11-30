5779016ec064482140e4394c0d4abfc6958d8a29
unknown <guiga.santos1711@gmail.com>
2022-11-30 00:08:22 -0300

    receive dhcp function

--- recv_raw.c
+++ recv_raw.c
@@ -14,6 +14,8 @@
 #define PROTO_UDP 17
 #define DST_PORT 8000
 
+int receive_dhcp_packet(struct eth_frame_s *socket, uint8_t *raw_buffer[ETH_LEN]);
+
 char bcast_mac[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 char dst_mac[6] = {0x00, 0x00, 0x00, 0x22, 0x22, 0x22};
 char src_mac[6] = {0x00, 0x00, 0x00, 0x33, 0x33, 0x33};
@@ -46,6 +48,50 @@ int main(int argc, char *argv[])
 
     /* End of configuration. Now we can receive data using raw sockets. */
 
+    receive_dhcp_packet(socket, raw_buffer);
+    // while (1){
+    //     numbytes = recvfrom(sockfd, raw_buffer, ETH_LEN, 0, NULL, NULL);
+    
+    //     if (raw->ethernet.eth_type == ntohs(ETH_P_IP)){
+
+    //         if (raw->ip.proto == 17){
+    //             unsigned int port_dest = (unsigned int) ntohs(raw->udp.dst_port);
+            
+    //             if(port_dest == 67 || port_dest == 68) {
+    //                 struct dhcp_hdr_s *dhcp = (struct dhcp_hdr_s *)&raw_buffer[sizeof(struct eth_hdr_s)+sizeof(struct ip_hdr_s)+sizeof(struct udp_hdr_s)];
+
+    //                 if(dhcp->op == 1)printf("eh DHCP request\n");
+
+    //                 printf("op type: %d = %d\n", raw->dhcp.op, dhcp->op);
+    //                 printf("msg type type: %d = %d\n", raw->dhcp.options[6], dhcp->options[6]);
+
+    //                 //dhcp->op == 1 -> dhcp request (discover/request)
+    //                 //dhcp->options[6] == 1(DHCPDISCOVER) -> dchp discover
+    //                 //dhcp->options[6] == 3(DHCPREQUEST) -> dchp request
+
+    //                 printf("IP packet, %d bytes - src ip: %d.%d.%d.%d dst ip: %d.%d.%d.%d proto: %d\n",
+    //                     numbytes,
+    //                     raw->ip.src[0], raw->ip.src[1], raw->ip.src[2], raw->ip.src[3],
+    //                     raw->ip.dst[0], raw->ip.dst[1], raw->ip.dst[2], raw->ip.dst[3],
+    //                     raw->ip.proto
+    //                 );
+
+    //                 printf("sucesso\n");
+    //             }else{
+    //                 printf("%d",port_dest);
+    //             }
+    //         }
+    //         continue;
+    //     }
+            
+        //printf("got a packet, %d bytes\n", numbytes);
+    }
+
+    return 0;
+}
+
+int receive_dhcp_packet(struct eth_frame_s socket, uint8_t raw_buffer[ETH_LEN]){
+
     while (1){
         numbytes = recvfrom(sockfd, raw_buffer, ETH_LEN, 0, NULL, NULL);
     
@@ -62,6 +108,10 @@ int main(int argc, char *argv[])
                     printf("op type: %d = %d\n", raw->dhcp.op, dhcp->op);
                     printf("msg type type: %d = %d\n", raw->dhcp.options[6], dhcp->options[6]);
 
+                    //dhcp->op == 1 -> dhcp request (discover/request)
+                    //dhcp->options[6] == 1(DHCPDISCOVER) -> dchp discover
+                    //dhcp->options[6] == 3(DHCPREQUEST) -> dchp request
+
                     printf("IP packet, %d bytes - src ip: %d.%d.%d.%d dst ip: %d.%d.%d.%d proto: %d\n",
                         numbytes,
                         raw->ip.src[0], raw->ip.src[1], raw->ip.src[2], raw->ip.src[3],
@@ -69,16 +119,9 @@ int main(int argc, char *argv[])
                         raw->ip.proto
                     );
 
-                    printf("sucesso\n");
-                }else{
-                    printf("%d",port_dest);
+                    // return 1;
                 }
             }
-            continue;
         }
-            
-        //printf("got a packet, %d bytes\n", numbytes);
     }
-
-    return 0;
 }
